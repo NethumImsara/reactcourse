3a190b1a2af74d5ea324979207617cab88f1bcb9
Nethum Imsara Hewaaarachchi <nethumimsara@gmail.com>
2024-08-26 19:56:05 +0530

    Done
    Done

--- src/Components/JobListings.jsx
+++ src/Components/JobListings.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import JobListing from './JobListing'
+import Spinner from './Spinner';
 
 
 const JobListings = ({isHome = false}) => {
@@ -9,8 +10,9 @@ const JobListings = ({isHome = false}) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
+      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
       try {
-        const res = await fetch('http://localhost:8000/jobs');
+        const res = await fetch(apiUrl);
         const data = await res.json();
         setJobs(data);
       } catch (error) {
@@ -20,24 +22,27 @@ const JobListings = ({isHome = false}) => {
       }
     };
 
-
     fetchJobs();
   }, []);
 
 
-  
   return (
     <section className="bg-blue-50 px-4 py-10">
     <div className="container-xl lg:container m-auto">
       <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
         {isHome ? 'Recent Jobs' : 'Browse Jobs'}
       </h2>
+      {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         {jobs.map((job)=>(
          <JobListing key={job.id} job={job}/>
+         
         ))}
-       
       </div>
+      )}
     </div>
   </section>
   )
